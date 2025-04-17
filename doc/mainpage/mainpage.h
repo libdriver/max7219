3d5ac97a9400e44837ebc224283c4f9672712604
lishifeng <lishifenging@outlook.com>
2025-04-18 00:24:28 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver MAX7219
- * @brief     LibDriver MAX7219 is a full function driver of MAX7219 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver MAX7219 is a full-featured driver of MAX7219 and it can run in both MCU and Linux platform.
  * @version   2.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2021/03/22  <td>2.0      <td>Shifeng Li  <td>format the code
